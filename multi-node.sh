b467193c2d2ed5279b5edaeeb490cc901d716c8c
Noooblien <rsm050501@gmail.com>
2023-10-05 18:25:37 +0530

    Fix bug in automated setup script for aircosmicEVM node
    - Resolved a bug that prevented the setup script from correctly initializing the aircosmicEVM node.
    - The script now functions as expected, automating node setup, copying genesis.json, setting persistent peers, and starting the node.
    - Tested locally; multi-node setup is now bug-free and operational.
    
    Author: Rahul Singh Maraskole

--- multi-node.sh
+++ multi-node.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Define variables
-SEED_NODE_IP="192.1.168.32:26656"
+SEED_NODE_IP="192.1.168.32"
 SEED_NODE_ID="d27015f43a1b5de64ecda0f99ac00a0de34f3d95"
 CHAIN_ID="aircosmic_5501-1107"
 NODE_NAME="aircosmic-node-1"
 
 # Initialize the Ethermint node with the specified chain ID
-ethermintd init $NODE_NAME --chain-id=$CHAIN_ID
+./build/ethermintd init $NODE_NAME --chain-id=$CHAIN_ID
 
 # Copy the genesis file from the seed node
 cp ./seed-genesis.json ~/.ethermintd/config/genesis.json
@@ -16,4 +16,4 @@ cp ./seed-genesis.json ~/.ethermintd/config/genesis.json
 sed -i "s/persistent_peers = \"\"/persistent_peers = \"$SEED_NODE_ID@$SEED_NODE_IP:26656\"/g" ~/.ethermintd/config/config.toml
 
 # Start the Ethermint node
-ethermintd start
+./build/ethermintd start
